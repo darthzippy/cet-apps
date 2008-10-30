00fda85a90551942e3573d553bed5e2e157cedd7
Eric Givens <eric@Macintosh-9.local>
2008-10-30 15:37:31 -0400

    Fixed GoogleChart gem in environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -26,7 +26,8 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "aws-s3", :lib => "aws/s3"
   # config.gem 'mislav-will_paginate', :version => '~> 2.3.2', :lib => 'will_paginate', :source => 'http://gems.github.com'
-
+  config.gem "googlecharts", :lib => "gchart"
+  
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
   # :all can be used as a placeholder for all plugins not explicitly named
@@ -66,5 +67,4 @@ Rails::Initializer.run do |config|
   # Activate observers that should always be running
   # config.active_record.observers = :cacher, :garbage_collector
   
-  require 'gchart'
 end
