cbf2bcf107026f162dcbea1269229973bfbaf1b4
Eric Givens <darthzippy@gmail.com>
2008-10-31 08:30:29 -0400

    Added packed gems for googlecharts and will_paginate

--- config/environment.rb
+++ config/environment.rb
@@ -25,7 +25,7 @@ Rails::Initializer.run do |config|
   # config.gem "bj"
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "aws-s3", :lib => "aws/s3"
-  # config.gem 'mislav-will_paginate', :version => '~> 2.3.2', :lib => 'will_paginate', :source => 'http://gems.github.com'
+  config.gem 'mislav-will_paginate', :lib => 'will_paginate', :source => 'http://gems.github.com'
   config.gem "googlecharts", :lib => "gchart"
   
   # Only load the plugins named here, in the order given. By default, all plugins 
