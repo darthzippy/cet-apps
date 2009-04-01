f2ffcf85c61e4cd2cc7287529cbe1193605497d0
Eric Givens <darthzippy@gmail.com>
2009-04-01 15:58:37 -0400

    Fixed buq with dvd/edit 'show' link

--- app/models/dvd.rb
+++ app/models/dvd.rb
@@ -10,7 +10,6 @@ class Dvd < ActiveRecord::Base
   validates_presence_of :quantity, :on => :create, :message => "can't be blank"
   validates_numericality_of :quantity, :on => :create, :message => "is not a number"
   validates_presence_of :pickup, :on => :create, :message => "can't be blank"
-  validates_presence_of :paid, :on => :create, :message => "can't be blank"
   
   def self.search(search, page)
     paginate :per_page => 15, :page => page,
