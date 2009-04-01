9ccc6dd72c6ce3fe62c07804cc078e74c7f2f514
Eric Givens <darthzippy@gmail.com>
2009-04-01 15:54:03 -0400

    Added to validations to dvd.rb

--- app/models/dvd.rb
+++ app/models/dvd.rb
@@ -1,4 +1,16 @@
 class Dvd < ActiveRecord::Base
+  validates_presence_of :firstname, :on => :create, :message => "can't be blank"
+  validates_presence_of :lastname, :on => :create, :message => "can't be blank"
+  validates_presence_of :address1, :on => :create, :message => "can't be blank"
+  validates_presence_of :city, :on => :create, :message => "can't be blank"
+  validates_presence_of :state, :on => :create, :message => "can't be blank"
+  validates_presence_of :zip, :on => :create, :message => "can't be blank"
+  validates_presence_of :email, :on => :create, :message => "can't be blank"
+  validates_presence_of :phone, :on => :create, :message => "can't be blank"
+  validates_presence_of :quantity, :on => :create, :message => "can't be blank"
+  validates_numericality_of :quantity, :on => :create, :message => "is not a number"
+  validates_presence_of :pickup, :on => :create, :message => "can't be blank"
+  validates_presence_of :paid, :on => :create, :message => "can't be blank"
   
   def self.search(search, page)
     paginate :per_page => 15, :page => page,
