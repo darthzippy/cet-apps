d2f0646f75877c68b7cfc7ad7ad18863cc3025fb
Eric Givens <darthzippy@gmail.com>
2009-04-24 13:20:09 -0400

    Removed Compass Initializer

--- config/initializers/compass.rb
+++ config/initializers/compass.rb
@@ -1,8 +1,8 @@
-require 'compass'
+#require 'compass'
 # If you have any compass plugins, require them here.
-Compass.configuration do |config|
-  config.project_path = RAILS_ROOT
-  config.sass_dir = "public/stylesheets"
-  config.css_dir = "public/stylesheets/compiled"
-end
-Compass.configure_sass_plugin!
+#Compass.configuration do |config|
+#  config.project_path = RAILS_ROOT
+#  config.sass_dir = "public/stylesheets"
+#  config.css_dir = "public/stylesheets/compiled"
+#end
+#Compass.configure_sass_plugin!
