dcf0fe7b788db93ccb9a7142d3be40fb46e55e8c
Eric Givens <darthzippy@gmail.com>
2009-08-25 16:59:19 -0400

    Remove db:backup from schedule.rb

--- config/schedule.rb
+++ config/schedule.rb
@@ -20,7 +20,6 @@
 # Learn more: http://github.com/javan/whenever
 
 every 1.day, :at => '7:00 am' do
-  rake "db:backup RAILS_ENV=production"
   rake "utils:bb:course_combine"
   rake "utils:wco:update_users"
 end
