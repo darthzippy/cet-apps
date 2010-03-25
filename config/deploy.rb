b638038f9eb307d8de5c0f7885c81967203d6f89
Eric Givens <ericgivens@erics-mac-pro.gordon.edu>
2010-03-25 15:31:39 -0400

    Added 'RAILS_ENV=production' to schedule.rb daily rake tasks

--- config/deploy.rb
+++ config/deploy.rb
@@ -49,8 +49,8 @@ end
 after "deploy", "deploy:cleanup"
 #after "deploy:cleanup", "deploy:update_crontab"
 
-Dir[File.join(File.dirname(__FILE__), '..', 'vendor', 'gems', 'hoptoad_notifier-*')].each do |vendored_notifier|
-  $: << File.join(vendored_notifier, 'lib')
-end
+#Dir[File.join(File.dirname(__FILE__), '..', 'vendor', 'gems', 'hoptoad_notifier-*')].each do |vendored_notifier|
+#  $: << File.join(vendored_notifier, 'lib')
+#end
 
-require 'hoptoad_notifier/capistrano'
+#require 'hoptoad_notifier/capistrano'
