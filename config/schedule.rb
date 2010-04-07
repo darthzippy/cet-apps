b1aa5785f970afa51e45ca577f2c816783b30175
Eric Givens <ericgivens@erics-mac-pro.gordon.edu>
2010-04-07 08:41:53 -0400

    Tweaked Whenever sdhedule.rb and deploy.rb

--- config/schedule.rb
+++ config/schedule.rb
@@ -19,7 +19,7 @@
 
 # Learn more: http://github.com/javan/whenever
 
-every 1.day, :at => '7:00 am' do
+every 1.day, :at => '9:00 am' do
   rake "db:backup"
   rake "utils:upload_backup"
   rake "utils:bb:course_combine"
