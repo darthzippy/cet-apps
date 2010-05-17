563dbae931f783ebd84992d00ae37118181790a8
Eric Givens <ericgivens@erics-mac-pro.gordon.edu>
2010-05-17 14:53:25 -0400

    Removed rake utils:bb:course_combine from schedule.rb for the summer

--- config/schedule.rb
+++ config/schedule.rb
@@ -22,7 +22,6 @@
 every 1.day, :at => '9:00 am' do
   rake "db:backup"
   rake "utils:upload_backup"
-  rake "utils:bb:course_combine"
   rake "utils:bb:course_combine_summer"
   rake "utils:wco:update_users"
 end
