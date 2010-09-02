9704249856d1d57b13da8b582f2eabdf412e0e75
Eric Givens <ericgivens@erics-mac-pro.gordon.edu>
2010-09-02 08:25:50 -0400

    Tweaked schedule.rb

--- config/schedule.rb
+++ config/schedule.rb
@@ -20,9 +20,8 @@
 # Learn more: http://github.com/javan/whenever
 
 every 1.day, :at => '7:00 am' do
+  rake "utils:wco:update_users"
   rake "db:backup"
   rake "utils:upload_backup"
-  rake "utils:bb:course_combine_summer"
   rake "utils:bb:course_combine"
-  rake "utils:wco:update_users"
 end
