31f1a047b8a77c2e4680f880ec4df0b2408f03a6
Eric Givens <ericgivens@erics-mac-pro.gordon.edu>
2010-11-22 09:57:18 -0500

    Add inventory2010 green bar check to Users/show

--- app/helpers/departments_helper.rb
+++ app/helpers/departments_helper.rb
@@ -1,7 +1,7 @@
 module DepartmentsHelper
   
   def inventory_percent
-    (Float(@computers.in_inventory.count)/Float(@computers.count))*100
+    (Float(@computers.in_inventory.count)/Float(@computers.count))*100 || 0
   end
 
 end
